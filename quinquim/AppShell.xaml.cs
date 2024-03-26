a60b7fb0ea9bf9013810a0d46d117edd1704efc4
anna <annaheloisa1809@gmail.com>
2024-03-26 08:50:22 -0300

    começo

---
+++ quinquim/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace quinquim;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ quinquim/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace quinquim;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
